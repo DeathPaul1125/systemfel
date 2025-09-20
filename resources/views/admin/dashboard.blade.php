11e45563c2ece9aa59129c0bf9ed8416f28333e9
Pablo <civernet@gmail.com>
2025-09-19 23:44:58 -0600

    Agregando data tables y wireui

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,12 +1,17 @@
-<x-admin-layout :breadcrumb="[
-        [
-            'name' => 'Dashboard',
-            'url' => route('admin.dashboard'),
-        ],
-        [
-            'name' => 'Home',
-            'url' => null,
-        ]
-    ]">
+<x-admin-layout :breadcrumbs="[
+    [
+        'name' => 'Dashboard',
+        'url' => route('admin.dashboard'),
+    ],
+    [
+        'name' => 'Pruebas',
+    ]
+]">
+
+    <x-slot name="actions">
+
+    </x-slot>
+
+    Dashboard Admin
 
 </x-admin-layout>
