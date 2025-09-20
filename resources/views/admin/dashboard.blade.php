1ca4b5def8c404038f3eae22b8c08100ab17a07f
Pablo <civernet@gmail.com>
2025-09-19 22:38:58 -0600

    Agregando Breadcrumbs

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,3 +1,12 @@
-<x-admin-layout>
+<x-admin-layout :breadcrumb="[
+        [
+            'name' => 'Dashboard',
+            'url' => route('admin.dashboard'),
+        ],
+        [
+            'name' => 'Home',
+            'url' => null,
+        ]
+    ]">
 
 </x-admin-layout>
